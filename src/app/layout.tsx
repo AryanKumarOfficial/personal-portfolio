a4ed3ed42ce9c916ad859e8d0ffc7ead3be50f3c
Aryan Kumar <aryanak9163@gmail.com>
2025-01-22 17:28:26 +0530

    adds google verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     return (
         <html lang="en">
         <Head>
+            <meta name="google-site-verification" content="jUxKK5RnkDthnwx_wzFQndhkbfd7X8doSNOrJ9_pXKA"/>
             <Script src={"https://kit.fontawesome.com/248f39001f.js"} crossOrigin="anonymous"></Script>
             <link rel="stylesheet" type='text/css'
                   href={"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"}/>
