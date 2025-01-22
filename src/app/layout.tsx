1cce3004f90b111482dda7bc912de3fa34abefac
Aryan Kumar <aryanak9163@gmail.com>
2025-01-22 17:33:14 +0530

    modifies google verification tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <Head>
+        <head>
             <meta name="google-site-verification" content="jUxKK5RnkDthnwx_wzFQndhkbfd7X8doSNOrJ9_pXKA"/>
+        </head>
+        <Head>
             <Script src={"https://kit.fontawesome.com/248f39001f.js"} crossOrigin="anonymous"></Script>
             <link rel="stylesheet" type='text/css'
                   href={"https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"}/>
