46b14dd7d2fff15e691fa3d398e6590eb060a6a0
Aryan Kumar <aryanak9163@gmail.com>
2026-03-01 03:20:13 +0530

    chore: project cleanup for fresh start

--- src/backend/schema/message.ts
+++
@@ -1,9 +0,0 @@
-import {z} from "zod";
-
-export const formSchema = z.object({
-    name: z.string().min(2, {message: "Name must be at least 2 characters long"}),
-    email: z.string().email({message: "Invalid email address"}),
-    message: z.string().min(10).max(5000),
-})
-
-export type FormSchema = z.infer<typeof formSchema>;
--- src/helpers/generators.ts
+++
@@ -1,7 +0,0 @@
-const generateSecretTokens: () => string = (): string => {
-    return [...Array(32)]
-        .map(() => Math.random().toString(36)[2])
-        .join('')
-}
-
-export {generateSecretTokens};
