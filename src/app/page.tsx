eb6e41b16b51e19012e5cf78faec43613f47e6e0
Aryan Kumar <aryanak9163@gmail.com>
2026-03-01 05:59:00 +0530

    ui: make homepage responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,35 @@
+import Github from "@/assets/icons/Github";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <section className="container mx-auto min-h-screen flex items-center">
-      <div className="max-w-3xl">
-        <p className="text-muted-foreground font-mono mb-2">
+    <section className="container max-w-6xl mx-auto min-h-screen flex items-center px-6 pt-24">
+      <div className="max-w-2xl">
+        <p className="text-muted-foreground font-mono mb-3 text-sm">
           Full-Stack Engineer
         </p>
 
-        <h1 className="text-5xl md:text-7xl font-bold tracking-tight">
+        <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold tracking-tight leading-tight">
           Aryan Kumar
         </h1>
 
-        <p className="mt-4 text-lg text-muted-foreground max-w-xl">
+        <p className="mt-6 text-muted-foreground text-base sm:text-lg leading-relaxed">
           I build scalable dashboards, SaaS platforms, and real-time web
           applications using Next.js, PostgreSQL, and modern backend systems.
         </p>
 
-        <div className="flex gap-4 mt-6">
-          <Button className="bg-primary text-primary-foreground px-6 py-3 rounded-lg">
-            View Projects
+        <div className="flex flex-wrap gap-4 mt-8">
+          <Button size={"lg"} asChild>
+            <Link href={"#projects"}>View Projects</Link>
           </Button>
 
-          <Button className="border px-6 py-3 rounded-lg">GitHub</Button>
+          <Button variant={"outline"} size={"lg"} asChild>
+            <a href="https://github.com/aryankumarofficial" target="_blank">
+              <Github/>
+              Github
+            </a>
+          </Button>
         </div>
       </div>
     </section>
